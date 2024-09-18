87be8821880c96cadb5ae4e8b4255ae9bafa8279
aria <aria.legion2@gmail.com>
2024-09-18 20:43:06 +0330

    added urls

--- jabama/customer_app/urls.py
+++ jabama/customer_app/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
+from .views import Login , Register, ReserveView , VillaSchdule , AllVilla
+
 
 urlpatterns = [
+    path("login/" , Login.as_view()),
+    path("reserve/" , ReserveView.as_view()),
+    path("register/" , Register.as_view()),
+    path("all-villa/" , AllVilla.as_view()),
+    path("schdule/" , VillaSchdule.as_view()),
 ]
